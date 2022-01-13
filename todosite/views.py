7f56ab7af97ed42968f6d4c6df503d13232ad31a
VanVuHoang <20471@wc.school.nz>
2022-01-13 23:37:20 +1300

    Cap nhat css

--- todosite/views.py
+++ todosite/views.py
@@ -31,12 +31,7 @@ def removetask(request, item_id):
     return redirect('todosite')
 
 def updatetask(request, item_id):
-    # item = Task.objects.get(id=item_id)
-    # item.title = "Updated"
-    # item.save()
-    # return redirect('todosite')
-    subitem = "???"
     item = Task.objects.get(id=item_id)
-    item.title = subitem
+    item.title = "Updated"
     item.save()
     return redirect('todosite')
